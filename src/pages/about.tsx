72c481782925c3deeacb0d3f09e58a54be495a6e
lamido tijjani <kinglamido@gmail.com>
2022-02-03 09:41:42 +0100

    feat(tsconfig.json, src folder): add relative path config and change import in src file

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,12 +1,11 @@
-import * as React from "react"
 import type { NextPage } from "next"
 import Container from "@mui/material/Container"
 import Typography from "@mui/material/Typography"
 import Box from "@mui/material/Box"
 import Button from "@mui/material/Button"
-import Link from "../components/Link"
-import ProTip from "../components/ProTip"
-import Copyright from "../components/Copyright"
+import Link from "components/Link"
+import ProTip from "components/ProTip"
+import Copyright from "components/Copyright"
 
 const About: NextPage = () => {
   return (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
-import * as React from "react"
 import type { NextPage } from "next"
 import Container from "@mui/material/Container"
 import Typography from "@mui/material/Typography"
 import Box from "@mui/material/Box"
-import Link from "../components/Link"
-import ProTip from "../components/ProTip"
-import Copyright from "../components/Copyright"
+import Link from "components/Link"
+import ProTip from "components/ProTip"
+import Copyright from "components/Copyright"
 
 const Home: NextPage = () => {
   return (
